5c34e1cdf4d928d07dab61863597ff83cf5b4bb2
ddb <81467937+Kim-ge@users.noreply.github.com>
2021-08-11 14:09:01 +0900

    Update 4주차_과제1.cpp

--- 4주차_과제1.cpp
+++ 4주차_과제1.cpp
@@ -1,22 +1,21 @@
-#include<iostream>
-#include<algorithm>
+#include <iostream>
 using namespace std;
 
 int main() {
-	int min1, min2;
-	int b1, b2, b3, d1, d2;
-	cin >> b1;
-	cin >> b2;
-	cin >> b3;
-	cin >> d1;
-	cin >> d2;
-	
-	min1 = min(b1, b2);
-	min1 = min(min1, b3);
-	min2 = min(d1, d2);
+    int menu[5];
 
-	cout << min1 + min2 - 50;
-
-	return 0;
-	
+    int min1 = 9999;
+    int min2 = 9999;
+    for (int i = 0; i < 3; i++) {
+        cin >> menu[i];
+        if (menu[i] < min1)
+            min1 = menu[i];
+    }
+    for (int i = 3; i < 5; i++)
+    {
+        cin >> menu[i];
+        if (menu[i] < min2)
+            min2 = menu[i];
+    }
+    cout << min1 + min2 - 50;
 }

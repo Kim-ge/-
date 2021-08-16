33648dbfc48b0b105367a85a23f1fa190c199b54
ddb <81467937+Kim-ge@users.noreply.github.com>
2021-08-16 15:03:56 +0900

    Update 3주차_과제1.cpp

--- 3주차_과제1.cpp
+++ 3주차_과제1.cpp
@@ -2,13 +2,14 @@
 using namespace std;
 
 int main() {
-	int T, A, B, C = 0;
+	int T= 0;
 
 	cin >> T;
 
 	for (int i = 0; i < T; i++) {
+		int A,B,C= 0;
 		cin >> A;
-		
+
 		for (int k = 0; k < A; k++) {
 			cin >> B;
 			C = C + B;

8865e5ecc47f9bc1fdfe12985838fa1e558e78b6
ddb <81467937+Kim-ge@users.noreply.github.com>
2021-08-18 21:50:53 +0900

    Update 5주차_과제1.cpp

--- 5주차_과제1.cpp
+++ 5주차_과제1.cpp
@@ -4,16 +4,16 @@ using namespace std;
 
 int main() {
 	int Tcase;
-	cin >> Tcase; //입력받을 케이스 개수 
+	cin >> Tcase; 
 	for (int i = 0; i <Tcase; i++) {
 		int num; 
 		string S;
-		cin >> num; //케이스 개수만큼 각각 반복할 횟수, 문자열을 반복해서 입력받기
+		cin >> num; 
 		cin >> S;
 
-		for (int k = 0; k < S.length(); k++) { //입력받은 문자열 길이만큼
-			for (int j = 0; j < num; j++) { //입력받은 반복 횟수만큼 문자열 출력
-				cout << S[k]; //각각의 문자들을 같은 횟수만큼 반복해야 하므로 중첩 for문 사용
+		for (int k = 0; k < S.length(); k++) { 
+			for (int j = 0; j < num; j++) { 
+				cout << S[k]; 
 			}
 		}
 		cout << endl;
